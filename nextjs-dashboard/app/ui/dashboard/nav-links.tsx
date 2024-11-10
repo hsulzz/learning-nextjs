23f4eeabe9b15fa0ba59e862233300d3e9ace058
hsulzz <xulu@ymail.ne.jp>
2024-11-10 21:27:41 +0900

    chapter 9 completed

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -22,8 +22,7 @@ const links = [
 ];
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log(pathname);
-  console.log(pathname === '/dashboard/invoices');
+
   return (
     <>
       {links.map((link) => {
